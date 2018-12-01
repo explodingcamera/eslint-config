221363e9273cec3d872be86190d6f3bec08be701
Henry Gressmann <mail@explodingcamera.com>
2018-12-01 21:16:55 +0100

    initial commit
    Signed-off-by: Henry Gressmann <mail@explodingcamera.com>

---
+++ README.md
@@ -0,0 +1,28 @@
+# ESLint shareable config for canX
+
+## Install
+
+```
+$ npm install -D -E eslint prettier @canx/eslint-config
+```
+
+## Usage
+
+Add some ESLint config to your `package.json`:
+
+```json
+{
+	"name": "my-awesome-project",
+	"eslintConfig": {
+		"extends": "@canx/eslint-config"
+	}
+}
+```
+
+Or to `.eslintrc.json`:
+
+```json
+{
+	"extends": "@canx/eslint-config"
+}
+```
---
+++ index.js
@@ -0,0 +1,63 @@
+module.exports = {
+	extends: ['xo', 'xo-react', 'prettier', 'prettier/react'],
+	parser: 'babel-eslint',
+	parserOptions: {
+		sourceType: 'module',
+		ecmaFeatures: {
+			experimentalObjectRestSpread: true,
+			experimentalDecorators: true,
+			jsx: true,
+		},
+	}
+	plugins: ['prettier', 'unicorn'],
+	rules: {
+		'prettier/prettier': ['error', {
+			"trailingComma": "all",
+			"useTabs": true,
+			"singleQuote": true,
+			"printWidth": 80
+		}],
+		'comma-dangle': ['error', 'always-multiline'],
+		'no-unused-expressions': 0,
+		'no-return-assign': ['error', 'except-parens'],
+		'lines-between-class-members': 0,
+		'react/jsx-child-element-spacing': 0,
+		'react/jsx-one-expression-per-line': 0,
+		'react/destructuring-assignment': 'never',
+		'react/forbid-component-props': [
+			'warn',
+			{
+				forbid: ['style'],
+			},
+		],
+		'react/no-danger': 'warn',
+		'react/forbid-dom-props': [
+			'warn',
+			{
+				forbid: ['style'],
+			},
+		],
+		'react/no-unescaped-entities': 0,
+		'react/jsx-max-props-per-line': [
+			'error',
+			{
+				maximum: 1,
+				when: 'multiline',
+			},
+		],
+		'react/no-deprecated': 'warn',
+		'react/jsx-sort-props': 0,
+		'unicorn/filename-case': [
+			'error',
+			{
+				case: 'kebabCase',
+			},
+		],
+		'unicorn/no-abusive-eslint-disable': 'warn',
+	},
+	settings: {
+		react: {
+			version: '16',
+		},
+	},
+};
