3479d8498817d596af5c16fed190e630211cfcda
Henry <mail@henrygressmann.de>
2021-05-10 17:44:59 +0200

    fix: remove annoying rules
    Signed-off-by: Henry <mail@henrygressmann.de>

--- base-config.js
+++ base-config.js
@@ -138,6 +138,7 @@ config.overrides[0] = {
 		'@typescript-eslint/member-ordering': 'off',
 		'@typescript-eslint/no-confusing-void-expression': ['off'],
 		'@typescript-eslint/no-unsafe-assignment': 'off',
+		'@typescript-eslint/no-unsafe-member-access': 'off',
 		'@typescript-eslint/no-unused-vars': [
 			'warn',
 			{
